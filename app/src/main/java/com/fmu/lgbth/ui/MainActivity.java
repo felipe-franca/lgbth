914b1551f76a540d2df1e268de396ed55b0fdcf0
felipe-franca <felipe.paulino57@gmail.com>
2023-03-23 16:27:10 -0300

    Initial commit

---
+++ app/src/main/java/com/fmu/lgbth/ui/MainActivity.java
@@ -0,0 +1,16 @@
+package com.fmu.lgbth.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.fmu.lgbth.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/fmu/lgbth/ui/SplashScreenActivity.java
@@ -0,0 +1,20 @@
+package com.fmu.lgbth.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.fmu.lgbth.R;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(com.fmu.lgbth.R.layout.activity_splash_screen);
+
+        Handler handler = new Handler();
+
+    }
+}
