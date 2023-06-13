49795f7c27c86ab081af5b2ee93a269cf306e72e
felipe-franca <felipe.paulino57@gmail.com>
2023-06-12 21:04:39 -0300

    handle non logge user and null favorites on favorites fragment

--- app/src/main/java/com/fmu/lgbth/ui/activity/FavoriteFragment.java
+++ app/src/main/java/com/fmu/lgbth/ui/activity/FavoriteFragment.java
@@ -16,6 +16,7 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.fmu.lgbth.R;
@@ -63,46 +64,62 @@ public class FavoriteFragment extends Fragment {
             ListView newsListView = view.findViewById(R.id.favorite_post_card);
 
             User user = getUser();
-            PostsApi api = new RestClient().getPostApi();
-            Call<List<Post>> call = api.getFavoritePosts(user.getId());
 
-            call.enqueue(new Callback<List<Post>>() {
-                @Override
-                public void onResponse(Call<List<Post>> call, Response<List<Post>> response) {
-                    List<Post> postList = response.body();
+            if (null == user) {
+                ProgressBar progressBar = view.findViewById(R.id.favorites_progress_bar);
+                progressBar.setVisibility(View.GONE);
 
-                    if (null == postList) {
-                        Toast.makeText(getContext(), "Nenhum post encontrado", Toast.LENGTH_SHORT).show();
-                        return;
+                TextView nonLoggedUser = view.findViewById(R.id.non_user_logged);
+                nonLoggedUser.setVisibility(View.VISIBLE);
+            } else {
+                PostsApi api = new RestClient().getPostApi();
+                Call<List<Post>> call = api.getFavoritePosts(user.getId());
+
+                call.enqueue(new Callback<List<Post>>() {
+                    @Override
+                    public void onResponse(Call<List<Post>> call, Response<List<Post>> response) {
+                        List<Post> postList = response.body();
+
+                        if (null == postList || postList.size() < 1) {
+                            Toast.makeText(getContext(), "Nenhum post encontrado", Toast.LENGTH_SHORT).show();
+
+                            ProgressBar progressBar = view.findViewById(R.id.favorites_progress_bar);
+                            progressBar.setVisibility(View.GONE);
+
+                            TextView nonUserFavorite = view.findViewById(R.id.non_favorites_existing);
+                            nonUserFavorite.setVisibility(View.VISIBLE);
+
+                            return;
+                        }
+
+                        HomePostsAdapter newsAdapter = new HomePostsAdapter(getContext(), postList);
+                        newsListView.setAdapter(newsAdapter);
+
+                        ProgressBar loading = view.findViewById(R.id.favorites_progress_bar);
+                        loading.setVisibility(View.GONE);
+                        newsListView.setVisibility(View.VISIBLE);
+
+                        newsListView.setOnItemClickListener((adapterView, view, i, l) -> {
+                            Intent intent = new Intent(getContext(), PostDetail.class);
+                            intent.putExtra("POSTID", postList.get(i).getId());
+
+                            startActivity(intent);
+
+                            ImageView cv = view.findViewById(R.id.news_favorite_icon);
+                            cv.setOnClickListener(v -> {
+                                unsetFavorite(postList.get(i).getId());
+                                newsAdapter.delete(i);
+                            });
+                        });
                     }
 
-                    HomePostsAdapter newsAdapter = new HomePostsAdapter(getContext(), postList);
-                    newsListView.setAdapter(newsAdapter);
-
-                    ProgressBar loading = view.findViewById(R.id.favorites_progress_bar);
-                    loading.setVisibility(View.GONE);
-                    newsListView.setVisibility(View.VISIBLE);
-
-                    newsListView.setOnItemClickListener((adapterView, view, i, l) -> {
-                        Intent intent = new Intent(getContext(), PostDetail.class);
-                        intent.putExtra("POSTID", postList.get(i).getId());
-
-                        startActivity(intent);
-
-                        ImageView cv = view.findViewById(R.id.news_favorite_icon);
-                        cv.setOnClickListener(v -> {
-                            unsetFavorite(postList.get(i).getId());
-                            newsAdapter.delete(i);
-                        });
-                    });
-                }
-
-                @Override
-                public void onFailure(Call<List<Post>> call, Throwable t) {
-                    t.printStackTrace();
-                    Toast.makeText(getContext(), "Ocorreu um erro ao carregar as 'News'", Toast.LENGTH_SHORT).show();
-                }
-            });
+                    @Override
+                    public void onFailure(Call<List<Post>> call, Throwable t) {
+                        t.printStackTrace();
+                        Toast.makeText(getContext(), "Ocorreu um erro ao carregar as 'News'", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
